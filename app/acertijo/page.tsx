d8fa52622fb6bd3b5172789860135782f588db3e
FidelBM <a01798199@tec.mx>
2024-06-06 19:13:59 -0600

    first commit

---
+++ app/acertijo/page.tsx
@@ -0,0 +1,272 @@
+"use client"
+import { useEffect, useState } from "react"; // Importa los hooks useEffect y useState
+import acertijo from "@/img/acertijo.png"; // Importa la imagen del acertijo
+import { motion } from "framer-motion"; // Importa los componentes motion
+import Image from "next/image"; // Importa el componente Image
+import axios from 'axios'; // Importa axios
+import { useRouter } from 'next/navigation'; // Importa el hook useRouter
+
+export default function Acertijo() {
+    const [seconds, setSeconds] = useState(60); // Nuevo estado para el tiempo
+    const [respuestaA, setRespuestaA] = useState(''); // Nuevo estado para la respuesta A
+    const [respuestaB, setRespuestaB] = useState(''); // Nuevo estado para la respuesta B
+    const [respuestaC, setRespuestaC] = useState(''); // Nuevo estado para la respuesta C
+    const [respuestaD, setRespuestaD] = useState(''); // Nuevo estado para la respuesta D
+    const [nombre, setNombre] = useState(''); // Nuevo estado para el nombre del usuario
+    const [mensajePerder, setMensajePerder] = useState(false); // Nuevo estado para el mensaje de perder
+    const [mensajeGanar, setMensajeGanar] = useState(false); // Nuevo estado para el mensaje de ganar
+    const [mensajeEmpezar, setMensajeEmpezar] = useState(true); // Nuevo estado para el mensaje de empezar
+    const [respuestaIncorrecta, setRespuestaIncorrecta] = useState(false); // Nuevo estado para la respuesta incorrecta
+    const [intervalId, setIntervalId] = useState<number | null>(null); // Nuevo estado para el identificador del intervalo
+    const router = useRouter(); // Inicializa el hook useRouter
+
+    // Define la función startTimer
+    const startTimer = () => {
+        // Inicializa el intervalo
+        let interval = setInterval(() => {
+            setSeconds(seconds => seconds - 1);
+        }, 1000) as unknown as number; // Afirmación de tipo
+
+        setIntervalId(interval); // Guardar el identificador del intervalo
+        setMensajeEmpezar(false); // Hide the start message
+    };
+
+    // Define el efecto useEffect
+    useEffect(() => {
+        // Verifica si el tiempo ha llegado a 0
+        if(seconds === 0){
+            setMensajePerder(true); // Muestra el mensaje de perder
+            if (intervalId !== null) {
+                clearInterval(intervalId); // Detener el tiempo si el tiempo ha llegado a 0
+            }
+        }
+    }, [seconds]);
+
+    // Define la función onSubmit
+    const onSubmit = (event : React.FormEvent) => {
+        event.preventDefault(); // Prevenir el comportamiento por defecto para que no recarge la página
+        // Verifica si las respuestas son correctas
+        if (
+            respuestaA === undefined || respuestaA === '' ||
+            respuestaB === undefined || respuestaB === '' ||
+            respuestaC === undefined || respuestaC === '' ||
+            respuestaD === undefined || respuestaD === ''
+        ) {
+            setRespuestaIncorrecta(true); // Muestra que la respuesta es incorrecta
+        } else {
+            // Verifica si las respuestas son correctas
+            if (parseInt(respuestaA) == 4 && parseInt(respuestaB) == 3 && parseInt(respuestaC) == 1 && parseInt(respuestaD) == 0) {
+                setMensajeGanar(true); // Muestra el mensaje de ganar
+                if (intervalId !== null) {
+                    clearInterval(intervalId); // Detener el tiempo si la respuesta es correcta
+                }
+            }else{
+                setRespuestaIncorrecta(true); // Muestra que la respuesta es incorrecta
+            }
+        }
+    }
+
+    // Define la función subirTiempo
+    const subirTiempo = async (event : React.FormEvent) => {
+        event.preventDefault(); // Prevenir el comportamiento por defecto para que no recarge la página
+        const tiempo = 60 - seconds; // Calcula el tiempo
+
+        // Crea un objeto usuario
+        const usuario = {
+            nombre: nombre,
+            tiempo: tiempo.toString()
+        }
+
+        // Sube el tiempo a la base de datos
+        try {
+            const response = await axios.post('http://localhost:8000/usuarios', usuario); // Llama a la base de datos
+            router.push('/'); // Redirige a la página principal
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center">
+            {mensajeEmpezar && (
+            <div
+                className="nunito flex h-full w-full items-center justify-center bg-[#80808080] absolute m-1 z-40 p-3 ">
+
+                <motion.div
+                    className="bg-white h-2/3 sm:h-1/2 md:w-1/2 w-full rounded-2xl flex flex-col items-center justify-center p-12"
+                    initial={{scale: 0.1}} // Tamaño inicial pequeño
+                    animate={{scale: [0, 1.1, 1]}} // Secuencia de tamaños: normal, más grande, normal
+                    transition={{duration: 0.3, times: [0, 0.8, 1]}}>
+                    <h1 className="md:text-3xl font-bold text-black m-3 text-2xl text-center">Como jugar</h1>
+                    <p className="text-black text-[18px] text-center"> A continuacion se te mostrara una suma, sin
+                        embargo falta descubrir algunos números.</p>
+                    <p className="text-black text-[18px] text-center"> Descubre el valor de los números faltantes y escribe sus valores, solo tienes 1 minuto.</p>
+                    <button
+                        onClick={() => startTimer()}
+                        className="learn-more mt-4">Empezar
+                    </button>
+                </motion.div>
+            </div>
+            )}
+            {mensajePerder && (
+                <div
+                    className="nunito flex h-full w-full items-center justify-center bg-[#80808080] absolute m-1 z-40 p-3 ">
+
+                    <motion.div
+                        className="bg-white h-2/3 sm:h-1/2 md:w-1/2 w-full rounded-2xl flex flex-col items-center justify-center p-12"
+                        initial={{scale: 0.1}} // Tamaño inicial pequeño
+                        animate={{scale: [0, 1.1, 1]}} // Secuencia de tamaños: normal, más grande, normal
+                        transition={{duration: 0.3, times: [0, 0.8, 1]}}>
+                        <h1 className="md:text-3xl font-bold text-black m-3 text-2xl text-center">¡Haz perdido!</h1>
+                        <p className="text-black text-[18px] text-center"> El tiempo se ha agotado y no descubriste la
+                            respuesta
+                            correcta.</p>
+                        <button
+                            onClick={() => location.reload()}
+                            className="learn-more mt-4">Reintentar
+                        </button>
+                    </motion.div>
+
+                </div>
+            )}
+
+            {mensajeGanar && (
+                <div className="nunito flex h-full w-full items-center justify-center bg-[#80808080] absolute m-1 z-40 p-3">
+
+                    <motion.div
+                        className="bg-white h-2/3 sm:h-1/2 md:w-1/2 w-full rounded-2xl flex flex-col items-center justify-center p-12"
+                        initial={{scale: 0.1}} // Tamaño inicial pequeño
+                        animate={{scale: [0, 1.1, 1]}} // Secuencia de tamaños: normal, más grande, normal
+                        transition={{duration: 0.3, times: [0, 0.8, 1]}}
+                    >
+                        <h1 className="text-3xl font-bold text-black m-3 text-center">¡Haz ganado!</h1>
+                        <p className="text-black text-[18px] text-center"> Felicidades haz resuelto el acertijo en {60 -seconds } segundos.</p>
+                        <p className="text-black text-[18px] text-center"> Ingresa tu nombre para guardar tu tiempo.</p>
+                        <form onSubmit={subirTiempo}>
+                            <div className="flex items-center space-x-2 m-4">
+                                <label className="min-w-max">
+                                    <p className="text-black text-[18px] text-center"> Nombre: </p>
+                                </label>
+                                <input
+                                    value={nombre}
+                                    onChange={(e) => setNombre(e.target.value)}
+                                    type="text"
+                                    placeholder="Nombre"
+                                    className="w-full border-gray-400 rounded border-[1px] border-stroke bg-transparent px-4 py-1 text-black outline-none transition focus:border-gray-400 active:border-gray-500"
+                                />
+                            </div>
+                            <div className="w-full flex justify-center text-center">
+                                <button
+
+                                    className="learn-more mt-4">Enviar
+                                </button>
+                            </div>
+                        </form>
+                    </motion.div>
+
+                </div>
+            )}
+            <motion.div
+                className="flex nunito min-h-screen flex-col items-center justify-center"
+                initial={{x: 2000}} // Posición inicial fuera de la vista
+                animate={{x: 0}} // Posición final en la vista
+                transition={{duration: 0.5}} // Duración de la transición
+            >
+                <p className={`text-[24px]  nunito m-3  `}> Tiempo
+                    restante: {seconds}</p>
+                <div className="progress-bar m-1"
+                     style={{width: `${seconds - 1}%`, backgroundColor: seconds <= 10 ? 'red' : '#4CAF50'}}></div>
+                <Image src={acertijo.src} alt="Acertijo" height={acertijo.height} width={acertijo.width}
+                       className="rounded md:h-2/5 md:w-2/5 m-2  h-1/3 w-[90%]"/>
+
+                <p className=" text-[18px] m-2">Ingresa la respuesta:</p>
+
+
+                <form className="text-[18px]">
+                    <div className="mb-4.5 m-1">
+                        <div className="max-w">
+                            <div className="flex items-center space-x-2 ">
+                                <label htmlFor="checkbox" className="min-w-max">
+                                    A =
+                                </label>
+                                <input
+                                    value={respuestaA}
+                                    onChange={(e) => setRespuestaA(e.target.value)}
+                                    type="number"
+                                    placeholder="Valor de A"
+                                    className="w-full border-gray-400 rounded border-[1px] border-stroke bg-transparent px-5 py-1 text-black outline-none transition focus:border-gray-400 active:border-gray-500"
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="mb-4.5 m-1">
+                        <div className="max-w">
+                            <div className="flex items-center space-x-2">
+                                <label htmlFor="checkbox" className="min-w-max">
+                                    B =
+                                </label>
+                                <input
+                                    value={respuestaB}
+                                    onChange={(e) => setRespuestaB(e.target.value)}
+                                    type="number"
+                                    placeholder="Valor de B"
+                                    className="w-full border-gray-400 rounded border-[1.5px] border-stroke bg-transparent px-5 py-1 text-black outline-none transition focus:border-gray-400 active:border-gray-500  "
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="mb-4.5 m-1">
+                        <div className="max-w">
+                            <div className="flex items-center space-x-2">
+                                <label htmlFor="checkbox" className="min-w-max">
+                                    C =
+                                </label>
+                                <input
+                                    value={respuestaC}
+                                    onChange={(e) => setRespuestaC(e.target.value)}
+                                    type="number"
+                                    placeholder="Valor de C"
+                                    className="w-full border-gray-400 rounded border-[1.5px] border-stroke bg-transparent px-5 py-1 text-black outline-none transition focus:border-gray-400 active:border-gray-500 "
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="mb-4.5 m-1">
+                        <div className="max-w">
+                            <div className="flex items-center space-x-2">
+                                <label htmlFor="checkbox" className="min-w-max">
+                                    D =
+                                </label>
+                                <input
+                                    value={respuestaD}
+                                    onChange={(e) => setRespuestaD(e.target.value)}
+                                    type="number"
+                                    placeholder="Valor de D"
+                                    className="w-full border-gray-400  rounded border-[1.5px] border-stroke bg-transparent px-5 py-1 text-black outline-none transition focus:border-gray-400 active:border-gray-500"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    {respuestaIncorrecta && <p className="text-red-500 text-center">Respuesta incorrecta</p>}
+
+                    <div className="w-full flex justify-center text-center">
+                        <button
+                            className="learn-more m-2 justify-center"
+                            onClick={onSubmit}
+                        >
+                            Subir
+                        </button>
+                    </div>
+
+                </form>
+
+
+            </motion.div>
+
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,107 @@
-import Image from "next/image";
+"use client"
+import { motion } from "framer-motion"; // Importa el componente motion
+import { useRouter } from 'next/navigation'; // Importa el hook useRouter
+import { useState, useEffect } from 'react'; // Importa los hooks useState y useEffect
+import axios from 'axios'; // Importa axios
+
+// Define la interfaz Usuario
+interface Usuario {
+    id: number;
+    nombre: string;
+    tiempo: string;
+}
+
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    const router = useRouter(); // Inicializa el hook useRouter
+    const [showPage, setShowPage] = useState(false); // Nuevo estado para controlar la animación
+    const [usuarios, setUsuarios] = useState<Usuario[]>([]); // Nuevo estado para almacenar los usuarios
+    const [isLoading, setIsLoading] = useState(true); // Nuevo estado para controlar la carga
+
+    // Define la función handleClick
+    const handleClick = () => {
+        setShowPage(true);
+    };
+
+    // Define el efecto useEffect
+    useEffect(() => {
+        // Define la función fetchUsers
+        const fetchUsers = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8000/usuarios`); // Llama a la base de datos
+                const sortedUsers = response.data.sort((a:Usuario, b:Usuario) => Number(a.tiempo) - Number(b.tiempo)); // Ordena los usuarios por tiempo
+                setUsuarios(sortedUsers); // Actualiza el estado usuarios
+                setIsLoading(false); // Actualiza el estado isLoading
+            } catch (error) {
+                console.error(error); // Muestra un error en la consola
+            }
+        };
+
+        fetchUsers(); // Llama a la función fetchUsers
+    }, []);
+
+    // Renderiza el componente
+    if (isLoading) {
+        return (
+            <div role="status" className="flex min-h-screen flex-col items-center justify-center">
+            <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin fill-blue-600"
+                 viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"/>
+                <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"/>
+            </svg>
+            <span className="sr-only">Loading...</span>
         </div>
-      </div>
+        );
+    } else {
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+        return (
+            <main className="flex min-h-screen flex-col items-center justify-center ">
+                <motion.div className="text-center"
+                            animate={{x: showPage ? -1000 : 0}} // Mueve el div fuera de la vista de la pantalla
+                            transition={{duration: 0.3}} // Duración de la animación
+                            onAnimationComplete={() => {
+                                if (showPage) {
+                                    router.push('/acertijo');
+                                }
+                            }}
+                >
+                    <h1 className="text-3xl nunito-bold">ACERTIJO</h1>
+                    <div className="m-20">
+                        <div className="relative inline-flex  group">
+                            <button
+                                className="learn-more nunito"
+                                onClick={handleClick}
+                            >
+                                Comenzar
+                            </button>
+                        </div>
+                    </div>
+                    <div>
+                        <h2 className="text-xl nunito-bold ">Top 5</h2>
+                        <table className="w-full text-center rtl:text-right text-gray-500 nunito">
+                            <thead className="text-xs text-gray-700 uppercase ">
+                            <tr className="px-6 py-3 text-[15px]">
+                                <th className="w-1/2">Nombre</th>
+                                <th className="w-1/2">Tiempo</th>
+                            </tr>
+                            </thead>
+                            <tbody className=" border-b ">
+                            {usuarios.slice(0, 5).map((usuario) => (
+                                <tr key={usuario.id}>
+                                    <td>{usuario.nombre}</td>
+                                    <td>{usuario.tiempo} segundos</td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </motion.div>
+            </main>
+        );
+    }
 }
